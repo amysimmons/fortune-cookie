47cc6d182ece0f8933eeb238709a61fc9e71e324
amysimmons <amy__simmons@hotmail.com>
2016-02-03 16:03:37 +1100

    iframe fix

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,4 +1,6 @@
 class FortunesController < ApplicationController
+  before_filter :allow_iframe_requests
+
   def home
 	  @fortune = Fortune.offset(rand(Fortune.count)).first
     @cookie_count = Fortune.count
@@ -25,12 +27,15 @@ class FortunesController < ApplicationController
     @tweet = @fortune.tweet
   end
 
+  def embed
+  end
+
   private
   def fortune_params
     params.require(:fortune).permit(:description, :name, :age, :city, :country, :twitter_handle)
   end
 
-  def allow_iframe
-    response.headers.except! 'X-Frame-Options'
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
   end
 end
